17b25eeb5267b6481320e1d59f86eef60a26d135
Papa <82730987+salartalaA@users.noreply.github.com>
2025-09-10 17:48:16 +0330

    Update weather.ts

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = import.meta.env.VITE_API_KEY;
+const API_KEY = import.meta.env.VITE_VISUALCROSSING_API_KEY;
 const BASE_URL =
   "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline";
 
