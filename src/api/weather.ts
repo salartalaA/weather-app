0f6f19fbe2a434de936aff10795ff2aa649e2bc0
salartalaa <salarmhmdn@gmail.com>
2025-09-10 16:35:47 +0430

    Fix: small adjusments

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "APQC6HQP7GFWKFR7B8ZQQ4MTA";
+const API_KEY = import.meta.env.VITE_API_KEY;
 const BASE_URL =
   "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline";
 
